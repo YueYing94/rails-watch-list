c2b779806d8e8509304e550deb37b263a218808b
yue ying <sarahyingyue1994@gmail.com>
2023-05-23 21:32:49 +0200

    need to check

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'bookmarks/new'
+  resources :lists do
+    resources :bookmarks, only:[:create]
+    resources :reviews, only:[:create]
+  end
+  resources :bookmarks, only:[:destroy]
 end
