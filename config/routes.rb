305524ac7f76a9c8862be52bf6e6dc0497d35bcd
yue ying <sarahyingyue1994@gmail.com>
2023-05-25 19:09:45 +0200

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'bookmarks/new'
+  root to: "pages#home_page"
   resources :lists do
     resources :bookmarks, only:[:create]
     resources :reviews, only:[:create]
